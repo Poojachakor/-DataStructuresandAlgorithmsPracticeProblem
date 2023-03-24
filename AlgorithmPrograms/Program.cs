02ebfd98e0658622679f8b7aefd51acc6b9fb946
Poojachakor <chakorpooja07@gmail.com>
2023-03-25 00:37:02 +0530

    Add: UC2 file

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int[] array = { 5, 0, 3, 9, 6, 2, 1 };
-            BubbleSort.doBubbleSort(array);
-            Console.WriteLine("Sorted Array");
-            foreach (int i in array)
-                Console.WriteLine(i + " ");
+
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.doInsertionSort();
+
+
         }
     }
 }
